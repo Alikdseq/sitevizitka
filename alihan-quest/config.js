7b530ea77bfd3bce3e4154380b75407cb3d56e6d
Алик Хан <alikhan2102@mail.ru>
2026-08-30 14:30:28 +0300

    chore: update tunnel URL to https://organization-blocked-mistress-usa.trycloudflare.com

--- alihan-quest/config.js
+++ alihan-quest/config.js
@@ -1,7 +1,7 @@
 ﻿// API backend URL. Auto-updated by start-alihan-quest launcher.
 window.QUEST_CONFIG = {
-  API_BASE: 'https://proceeding-inflation-invite-pubs.trycloudflare.com',
+  API_BASE: 'https://organization-blocked-mistress-usa.trycloudflare.com',
   DEMO_TOKEN: 'demo-alihan-quest',
-  CACHE_VERSION: 9,
+  CACHE_VERSION: 10,
 };
 
