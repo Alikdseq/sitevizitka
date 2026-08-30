60881af139df4fbadcf6fa2b761bc10eca653fe2
Алик Хан <alikhan2102@mail.ru>
2026-08-30 14:27:17 +0300

    chore: update tunnel URL to https://proceeding-inflation-invite-pubs.trycloudflare.com

--- alihan-quest/config.js
+++ alihan-quest/config.js
@@ -1,7 +1,7 @@
 ﻿// API backend URL. Auto-updated by start-alihan-quest launcher.
 window.QUEST_CONFIG = {
-  API_BASE: 'https://earlier-scope-amongst-cities.trycloudflare.com',
+  API_BASE: 'https://proceeding-inflation-invite-pubs.trycloudflare.com',
   DEMO_TOKEN: 'demo-alihan-quest',
-  CACHE_VERSION: 5,
+  CACHE_VERSION: 9,
 };
 
