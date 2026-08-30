598eea9bca72d9830e96fb0e46d0e87caf35df4b
Алик Хан <alikhan2102@mail.ru>
2026-08-30 18:49:47 +0300

    chore(alihan-quest): sync mini app + tunnel https://beast-shannon-richards-opening.trycloudflare.com

--- alihan-quest/config.js
+++ alihan-quest/config.js
@@ -1,6 +1,7 @@
 ﻿// API backend URL. Auto-updated by start-alihan-quest launcher.
 window.QUEST_CONFIG = {
-  API_BASE: 'https://circular-virtual-slowly-themes.trycloudflare.com',
+  API_BASE: 'https://beast-shannon-richards-opening.trycloudflare.com',
   DEMO_TOKEN: 'demo-alihan-quest',
-  CACHE_VERSION: 32,
+  CACHE_VERSION: 33,
 };
+
