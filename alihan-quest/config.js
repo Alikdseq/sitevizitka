be1f0072b124d3e5331191ba045476ab9bbdbf6e
Алик Хан <alikhan2102@mail.ru>
2026-09-06 14:12:52 +0300

    chore(alihan-quest): sync mini app + tunnel https://brake-nashville-fairfield-vertical.trycloudflare.com

--- alihan-quest/config.js
+++ alihan-quest/config.js
@@ -1,7 +1,7 @@
 ﻿// API backend URL. Auto-updated by start-alihan-quest launcher.
 window.QUEST_CONFIG = {
-  API_BASE: 'https://salt-horse-matters-feel.trycloudflare.com',
+  API_BASE: 'https://brake-nashville-fairfield-vertical.trycloudflare.com',
   DEMO_TOKEN: 'demo-alihan-quest',
-  CACHE_VERSION: 62,
+  CACHE_VERSION: 63,
 };
 
