bda65123b0d9a588187a8ce4f93f46e421eaa419
Алик Хан <alikhan2102@mail.ru>
2026-09-08 06:41:43 +0300

    chore(alihan-quest): sync mini app + tunnel https://wild-sic-exit-survey.trycloudflare.com

--- alihan-quest/config.js
+++ alihan-quest/config.js
@@ -1,7 +1,7 @@
 ﻿// API backend URL. Auto-updated by start-alihan-quest launcher.
 window.QUEST_CONFIG = {
-  API_BASE: 'https://grande-announces-instance-visible.trycloudflare.com',
+  API_BASE: 'https://wild-sic-exit-survey.trycloudflare.com',
   DEMO_TOKEN: 'demo-alihan-quest',
-  CACHE_VERSION: 79,
+  CACHE_VERSION: 80,
 };
 
